e49ffd232f5e2babbda3c77fd9f9a405f6592df6
max <maxwellvolz@gmail.com>
2016-01-09 17:49:58 -0800

    player controller initialized

---
+++ playerControllerScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript: MonoBehavious
+{
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+	
+	void Start ()
+	{}
+
+	void FixedUpdate ()
+	{
+		float move = input.GetAxis("Horizontal");
+	
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+		
+		if(move > && !facingRight)
+			Flip ();
+		else if(move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
+
